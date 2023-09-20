af555245857c99478e484f412b0c62e05ebfe09e
Olivier Le Merdy <olivier.lemerdy@kry.se>
2023-09-20 10:38:06 +0200

    Initial commit

---
+++ src/main/java/se/kry/demo/contract/producer/PlatformHackdaysCloudContractProducerApplication.java
@@ -0,0 +1,13 @@
+package se.kry.demo.contract.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatformHackdaysCloudContractProducerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PlatformHackdaysCloudContractProducerApplication.class, args);
+  }
+
+}
---
+++ src/test/java/se/kry/demo/contract/producer/PlatformHackdaysCloudContractProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package se.kry.demo.contract.producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatformHackdaysCloudContractProducerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
